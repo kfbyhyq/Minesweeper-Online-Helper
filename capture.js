c2eb9cf62e6bcc6f1231a3a58977586ede8c18a4
kfbyhyq <ymyq1525@163.com>
2024-12-24 16:02:34 +0800

    v3.5

--- capture.js
+++ capture.js
@@ -1,13 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('buttonSave').addEventListener('click', function () {
         const button = document.getElementById('buttonSave');
-        button.style.backgroundColor = '#ff9f18';   // 对应按钮变为橙色，表示运行中
+        button.style.backgroundColor = '#4caf50';
         chrome.tabs.query({ active: true, currentWindow: true }, function (tab1) {
             const tabId = tab1[0].id;
             chrome.scripting.executeScript({
                 target: { tabId },
                 function: function () {
                     var board = [];
+                    var mineNum = 0;
+
+                    const mineNum100 = document.querySelector("#top_area_mines_100").className;
+                    const match100 = mineNum100.match(/hd_top-area-num(\d+)$/);
+                    if (match100) {
+                        mineNum += parseInt(match100[1].slice(-1))*100;
+                    }
+                    const mineNum10 = document.querySelector("#top_area_mines_10").className;
+                    const match10 = mineNum10.match(/hd_top-area-num(\d+)$/);
+                    if (match10) {
+                        mineNum += parseInt(match10[1].slice(-1))*10;
+                    }
+                    const mineNum1 = document.querySelector("#top_area_mines_1").className;
+                    const match1 = mineNum1.match(/hd_top-area-num(\d+)$/);
+                    if (match1) {
+                        mineNum += parseInt(match1[1].slice(-1));
+                    }
+
                     const area = document.querySelector("#AreaBlock");
                     if (area) {
                         for (const cell of area.children) {
@@ -21,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
                                 }
                                 if (cell.classList.contains('hd_flag')) {
                                     board[row][col] = 'F';
+                                    mineNum++;
                                 } else if (cell.classList.contains('hd_closed')) {
                                     board[row][col] = 'H';
                                 } else {
@@ -30,10 +49,11 @@ document.addEventListener('DOMContentLoaded', function() {
                             }
                     }
                     console.log(board);
-                    saveAsMine(board, 'board.mine');
+                    console.log(mineNum);
+                    saveAsMine(board, mineNum, 'board.mine');
 
-                    function saveAsMine(board, filename) {
-                        let text = '30x16x99\n';
+                    function saveAsMine(board, mineNum, filename) {
+                        let text = board[0].length + 'x' + board.length + 'x' + mineNum + '\n';
                         for (let i = 0; i < board.length; i++) {
                             for (let j = 0; j < board[i].length; j++) {
                                 text += board[i][j];
