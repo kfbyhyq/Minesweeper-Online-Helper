54fc89c229dcac4bf8a68ad991177454fbdb0c54
kfbyhyq <ymyq1525@163.com>
2025-02-17 16:39:54 +0800

    v3.9.11

--- monitorChat.js
+++ monitorChat.js
@@ -51,7 +51,8 @@ document.addEventListener('DOMContentLoaded', function() {
                                 (!message.includes('rading') && !message.includes('rade') && !message.includes('RADE') && !message.includes('RADING') &&
                                 !message.includes('ell') && !message.includes('ELL') && !message.includes('ale') && !message.includes('ALE') && 
                                 !message.includes('mc') && !message.includes('MC') && !message.includes('Mc') && !message.includes('xchange') && !message.includes('XCHANGE') && 
-                                !message.includes('/h') && !message.includes('/p') && !message.includes('/ep') && !message.includes('/l') && !message.includes('/ ') && 
+                                !message.includes('/h') && !message.includes('/p') && !message.includes('/e') && !message.includes('/l') && !message.includes('/ ') && 
+                                !message.includes('/H') && !message.includes('/P') && !message.includes('/E') && !message.includes('/L') && !message.includes('/ ') && 
                                 (message.includes('Win') || message.includes('Earn') || message.includes('Find') || message.includes('Complete') ||
                                 message.includes('获得') || message.includes('完成') || message.includes('获取') || message.includes('达到') || 
                                 message.includes('獲得'))))) {
